436e4826b29e09d6c9c1d66085bbe3ef97d04c6a
Ty Thomasson <tythomasson@outlook.com>
2024-10-04 08:31:34 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden'); // Toggle visibility of the menu
+});
